c9b020cb58b28445a3a7a45d4e2b966319173ae5
Viktor Kulikov <bezdoom@gmail.com>
2012-02-16 15:12:21 +0400

    twitter crossposting class

---
+++ root/classes/helpers/Twitter.php
@@ -0,0 +1,45 @@
+<?php 
+
+class Twitter {
+
+	private $username = "Sochi2014OC";
+	private $password = "bobsleigh1";
+	private $last_error = null;
+
+	public function addMessage($message)
+	{
+		if ($message) {
+			$context = stream_context_create(array(
+			    'http' => array(
+				'method'  => 'POST',
+				'header'  => sprintf("Authorization: Basic %s\r\n", base64_encode($this->username.':'.$this->password)).
+				                   "Content-type: application/x-www-form-urlencoded\r\n",
+				'content' => http_build_query(array('status' => $message)),
+				'timeout' => 5,
+			),
+			));
+
+			$result_xml = file_get_contents('http://twitter.com/statuses/update.xml', false, $context);
+			if (!$result_xml) {
+				$this->last_error = "Пустой ответ";
+				return false;
+			} else {
+				try {
+					$xml = new SimpleXMLElement($result_xml);
+					if($xml->id) {
+						return $xml->id;
+					} else {
+						return false;
+					}
+				} catch (Exception $e) {
+					$this->last_error = '$xml is not a valid xml string: '.$result_xml;
+				}
+			}
+		}
+	}
+
+	public function getLastError () {
+		return $this->last_error;
+	}
+
+}
